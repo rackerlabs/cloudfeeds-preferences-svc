38e2f52127a499ab611b1fd55bbef58f6c478062
Alex Schultz <aschultz@next-development.com>
2014-11-25 16:10:17 -0600

    fixing group name

--- src/main/resources/postinstall.sh
+++ src/main/resources/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 USERNAME=prefssvc
-GROUP=prefsvc
+GROUP=prefssvc
 LOG_DIR=/var/log/preferences-service
 /sbin/chkconfig --add preferences-service
 test -d $LOG_DIR || ( mkdir $LOG_DIR && chown $USERNAME:$GROUP $LOG_DIR )
