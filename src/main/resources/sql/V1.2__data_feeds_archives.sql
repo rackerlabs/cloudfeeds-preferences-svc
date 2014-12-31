10aa454443714ec22c9972003a6bd0799474fcb7
chan5120 <chandra.addala@rackspace.com>
2014-12-31 15:46:26 -0600

    [CF-340] Flyway project   - Modified schema to have tables names and column names to be in lower case   - Added sql to insert metadata for cloud feeds   - Added code to generate i   - Added maven plugin to generate and public artifact(zip file) to local maven repository

---
+++ src/main/resources/sql/V1.2__data_feeds_archives.sql
@@ -0,0 +1,22 @@
+insert
+  into "preferences_metadata" ("slug", "description", "schema")
+values ('archive_prefs',
+        'Cloud feeds Archive Preferences',
+        'object {
+            boolean enabled;
+            array [
+              string [ "JSON", "XML" ];
+            ] {1,2} data_format;
+            string  default_container_name = "FeedsArchives";
+            object {
+              string iad;
+              string ord;
+              string dfw;
+              string lon;
+              string hkg;
+              string syd;
+            } archive_container_urls?;
+        };');
+
+
+
