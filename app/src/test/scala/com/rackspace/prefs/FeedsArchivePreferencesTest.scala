7e7eb8063324982335986019838fcea91e9f6faf
Vinny Ly <vinny.ly@rackspace.com>
2015-04-20 23:48:28 -0500

    CF-601 reorganize tests in FeedsArchivePreferencesTest.scala

--- app/src/test/scala/com/rackspace/prefs/FeedsArchivePreferencesTest.scala
+++ app/src/test/scala/com/rackspace/prefs/FeedsArchivePreferencesTest.scala
@@ -21,43 +21,98 @@ class FeedsArchivePreferencesTest extends ScalatraSuite with FunSuiteLike with I
     val db = Database.forDataSource(new ComboPooledDataSource)
     addServlet(new PreferencesService(db), "/*")
 
+    val prefs_enable_all =
+    """
+      |{
+      |  "enabled": true,
+      |  "data_format" : [ "JSON", "XML" ],
+      |  "default_archive_container_url" : "https://storage.stg.swift.racklabs.com/v1/StagingUS_6b881249-b992-44ef-9ad1-2b9f5107d2f9/FeedsArchives",
+      |  "archive_container_urls": {
+      |      "iad": "http://...",
+      |      "dfw": "http://...",
+      |      "ord": "http://...",
+      |      "lon": "http://...",
+      |      "hkg": "http://...",
+      |      "syd": "http://..."
+      |  }
+      |}
+    """.stripMargin
+
+    val prefs_enable_regions =
+    """
+      |{
+      |  "enabled": true,
+      |  "data_format" : [ "JSON", "XML" ],
+      |  "archive_container_urls": {
+      |      "iad": "http://...",
+      |      "dfw": "http://...",
+      |      "ord": "http://...",
+      |      "lon": "http://...",
+      |      "hkg": "http://...",
+      |      "syd": "http://..."
+      |  }
+      |}
+    """.stripMargin
+
+    val prefs_enable_all_json =
+    """
+      |{
+      |  "enabled": true,
+      |  "data_format" : [ "JSON" ],
+      |  "default_archive_container_url" : "https://storage.stg.swift.racklabs.com/v1/StagingUS_6b881249-b992-44ef-9ad1-2b9f5107d2f9/FeedsArchives",
+      |  "archive_container_urls": {
+      |      "iad": "http://...",
+      |      "dfw": "http://...",
+      |      "ord": "http://...",
+      |      "lon": "http://...",
+      |      "hkg": "http://...",
+      |      "syd": "http://..."
+      |  }
+      |}
+    """.stripMargin
+
+    val prefs_disable_all =
+    """
+      |{
+      |  "enabled": false,
+      |  "data_format" : [ "JSON", "XML" ],
+      |  "default_archive_container_url" : "https://storage.stg.swift.racklabs.com/v1/StagingUS_6b881249-b992-44ef-9ad1-2b9f5107d2f9/FeedsArchives",
+      |  "archive_container_urls": {
+      |      "iad": "http://...",
+      |      "dfw": "http://...",
+      |      "ord": "http://...",
+      |      "lon": "http://...",
+      |      "hkg": "http://...",
+      |      "syd": "http://..."
+      |  }
+      |}
+    """.stripMargin
+
     override def beforeAll {
         super.beforeAll
         clearData(db)
     }
 
     test("should get 200: GET /archive/:id") {
-        // declaring this randomId outside test() as a var
-        // doesn't work with subsequent tests :-(
         val randomId = Random.nextInt()
-        val prefs = """
-                      |{
-                      |  "enabled": true,
-                      |  "data_format" : [ "JSON", "XML" ],
-                      |  "default_archive_container_url" : "https://storage.stg.swift.racklabs.com/v1/StagingUS_6b881249-b992-44ef-9ad1-2b9f5107d2f9/FeedsArchives",
-                      |  "archive_container_urls": {
-                      |      "iad": "http://...",
-                      |      "dfw": "http://...",
-                      |      "ord": "http://...",
-                      |      "lon": "http://...",
-                      |      "hkg": "http://...",
-                      |      "syd": "http://..."
-                      |  }
-                      |}
-                    """.stripMargin
-        createPreference(db, randomId.toString(), "archive", prefs)
+        createPreference(db, randomId.toString(), "archive", prefs_enable_all)
 
         info("Calling GET /archive/" + randomId)
         get("/archive/" + randomId) {
             status should equal (200)
-            body should equal (prefs)
+            body should equal (prefs_enable_all)
             response.mediaType should equal (Some("application/json"))
         }
+    }
+
+    test("should get 200: GET /archive/:id/") {
+        val randomId = Random.nextInt()
+        createPreference(db, randomId.toString(), "archive", prefs_enable_all)
 
         info("Calling GET /archive/" + randomId + "/")
         get("/archive/" + randomId + "/") {
             status should equal (200)
-            body should equal (prefs)
+            body should equal (prefs_enable_all)
             response.mediaType should equal (Some("application/json"))
         }
     }
@@ -79,32 +134,8 @@ class FeedsArchivePreferencesTest extends ScalatraSuite with FunSuiteLike with I
 
     test("should get 201: POST of a new good preferences to /archive/:id") {
         val randomId = Random.nextInt()
-        val prefs =
-            """
-              |{
-              |  "enabled": true,
-              |  "data_format" : [ "JSON", "XML" ],
-              |  "archive_container_urls": {
-              |      "iad": "http://...",
-              |      "dfw": "http://...",
-              |      "ord": "http://...",
-              |      "lon": "http://...",
-              |      "hkg": "http://...",
-              |      "syd": "http://..."
-              |  }
-              |}
-            """.stripMargin
         info("Calling POST /archive/" + randomId)
-        post("/archive/" + randomId, prefs, Map("Content-Type" -> "application/json")) {
-            if ( status != 201 ) {
-                info(body)
-            }
-            status should equal (201)
-        }
-
-        val nextRandomId = randomId + 1
-        info("Calling POST /archive/" + nextRandomId + "/")
-        post("/archive/" + nextRandomId + "/", prefs, Map("Content-Type" -> "application/json")) {
+        post("/archive/" + randomId, prefs_enable_regions, Map("Content-Type" -> "application/json")) {
             if ( status != 201 ) {
                 info(body)
             }
@@ -112,39 +143,36 @@ class FeedsArchivePreferencesTest extends ScalatraSuite with FunSuiteLike with I
         }
     }
 
+    test("should get 201: POST of a new good preferences to /archive/:id/") {
+        val randomId = Random.nextInt()
+        info("Calling POST /archive/" + randomId + "/")
+        post("/archive/" + randomId + "/", prefs_enable_regions, Map("Content-Type" -> "application/json")) {
+          if ( status != 201 ) {
+            info(body)
+          }
+          status should equal (201)
+        }
+    }
+
     test("should get 201: POST of a new preferences with only one data_format to /archive/:id") {
         val randomId = Random.nextInt()
-        val prefs =
-            """
-              |{
-              |  "enabled": true,
-              |  "data_format" : [ "JSON" ],
-              |  "default_archive_container_url" : "https://storage.stg.swift.racklabs.com/v1/StagingUS_6b881249-b992-44ef-9ad1-2b9f5107d2f9/FeedsArchives",
-              |  "archive_container_urls": {
-              |      "iad": "http://...",
-              |      "dfw": "http://...",
-              |      "ord": "http://...",
-              |      "lon": "http://...",
-              |      "hkg": "http://...",
-              |      "syd": "http://..."
-              |  }
-              |}
-            """.stripMargin
         info("Calling POST /archive/" + randomId)
-        post("/archive/" + randomId, prefs, Map("Content-Type" -> "application/json")) {
+        post("/archive/" + randomId, prefs_enable_all_json, Map("Content-Type" -> "application/json")) {
             if ( status != 201 ) {
                 info(body)
                 println(body)
             }
             status should equal (201)
         }
+    }
 
-        val nextRandomId = randomId + 1
-        info("Calling POST /archive/" + nextRandomId + "/")
-        post("/archive/" + nextRandomId + "/", prefs, Map("Content-Type" -> "application/json")) {
+    test("should get 201: POST of a new preferences with only one data_format to /archive/:id/") {
+        val randomId = Random.nextInt()
+        info("Calling POST /archive/" + randomId + "/")
+        post("/archive/" + randomId + "/", prefs_enable_all_json, Map("Content-Type" -> "application/json")) {
             if ( status != 201 ) {
-              info(body)
-              println(body)
+                info(body)
+                println(body)
             }
             status should equal (201)
         }
@@ -152,58 +180,37 @@ class FeedsArchivePreferencesTest extends ScalatraSuite with FunSuiteLike with I
 
     test("should get 200: POST of a good preferences to existing /archive/:id") {
         val randomId = Random.nextInt()
-        val prefs_disabled =
-          """
-            |{
-            |  "enabled": false,
-            |  "data_format" : [ "JSON", "XML" ],
-            |  "default_archive_container_url" : "https://storage.stg.swift.racklabs.com/v1/StagingUS_6b881249-b992-44ef-9ad1-2b9f5107d2f9/FeedsArchives",
-            |  "archive_container_urls": {
-            |      "iad": "http://...",
-            |      "dfw": "http://...",
-            |      "ord": "http://...",
-            |      "lon": "http://...",
-            |      "hkg": "http://...",
-            |      "syd": "http://..."
-            |  }
-            |}
-          """.stripMargin
 
         info("Calling 1st POST /archive/" + randomId)
-        post("/archive/" + randomId, prefs_disabled, Map("Content-Type" -> "application/json")) {
+        post("/archive/" + randomId, prefs_disable_all, Map("Content-Type" -> "application/json")) {
             if ( status != 201 ) {
                 info(body)
             }
             status should equal (201)
         }
 
-        val prefs_enabled =
-          """
-            |{
-            |  "enabled": true,
-            |  "data_format" : [ "JSON", "XML" ],
-            |  "default_archive_container_url" : "https://storage.stg.swift.racklabs.com/v1/StagingUS_6b881249-b992-44ef-9ad1-2b9f5107d2f9/FeedsArchives",
-            |  "archive_container_urls": {
-            |      "iad": "http://...",
-            |      "dfw": "http://...",
-            |      "ord": "http://...",
-            |      "lon": "http://...",
-            |      "hkg": "http://...",
-            |      "syd": "http://..."
-            |  }
-            |}
-          """.stripMargin
-
         info("Calling 2nd POST /archive/" + randomId)
-        post("/archive/" + randomId, prefs_enabled, Map("Content-Type" -> "application/json")) {
+        post("/archive/" + randomId, prefs_enable_all, Map("Content-Type" -> "application/json")) {
             if ( status != 200 ) {
                 info(body)
             }
             status should equal (200)
         }
+    }
 
-        info("Calling 3rd POST /archive/" + randomId + "/")
-        post("/archive/" + randomId + "/", prefs_enabled, Map("Content-Type" -> "application/json")) {
+    test("should get 200: POST of a good preferences to existing /archive/:id/") {
+        val randomId = Random.nextInt()
+
+        info("Calling 1st POST /archive/" + randomId + "/")
+        post("/archive/" + randomId + "/", prefs_disable_all, Map("Content-Type" -> "application/json")) {
+            if ( status != 201 ) {
+                info(body)
+            }
+            status should equal (201)
+        }
+
+        info("Calling 2nd POST /archive/" + randomId + "/")
+        post("/archive/" + randomId + "/", prefs_enable_all, Map("Content-Type" -> "application/json")) {
             if ( status != 200 ) {
                 info(body)
             }
